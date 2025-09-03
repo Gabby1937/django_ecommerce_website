3bac03f6382475828eb2c8a5400602b519827a43
Gabby1937 <gabrieljohnson1937@gmail.com>
2025-09-03 16:46:49 +0100

    Extended the User Profile

--- ecom/store/admin.py
+++ ecom/store/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Category, Customer, Product, Order
-
+from .models import Category, Customer, Product, Order, Profile
+from django.contrib.auth.models import User
 # Register your models here.
 
 
@@ -8,3 +8,23 @@ admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Order)
+admin.site.register(Profile)
+
+
+# Mix Profile with User
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    # can_delete = False
+    # verbose_name_plural = 'Profile'
+    
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = ['username', 'first_name', 'last_name', 'email']
+    inlines = [ProfileInline]
+    
+# Unregister User Model the old way
+admin.site.unregister(User)
+
+# Re-register User the new way
+admin.site.register(User, UserAdmin)
