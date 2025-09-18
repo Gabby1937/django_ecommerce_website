9244ea661b18e610d88f88721b81419067036068
Gabby1937 <gabrieljohnson1937@gmail.com>
2025-09-18 21:30:25 +0100

    Fix STATIC_ROOT to use os.path.join

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = ['static/']
 
 # White noise static stuff
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Fixed: Use os.path.join
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
